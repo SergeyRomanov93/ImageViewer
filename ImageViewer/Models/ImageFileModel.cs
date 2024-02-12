60d79bc61025a725c5a6df5b2201668e645d84e1
Sergey Romanov <romanov.s@isbc.ru>
2024-02-12 12:04:39 +0300

    Добавлено увеличение и просмотр картинки

--- ImageViewer/Models/ImageFileModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ImageViewer.Models
-{
-    public class ImageFileModel
-    {
-        public string Path { get; set; }
-    }
-}
--- ImageViewer/ViewModels/MainWindowViewModel.cs
+++ ImageViewer/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,66 @@
 ﻿using ImageViewer.Command;
 using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
+using System.Windows.Media.Imaging;
 
 namespace ImageViewer.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _photosDirectory;
-        private List<string> _photos;
-        private string _filename;
+        private static readonly string[] _supportedExtensions = new[] 
+        { 
+            ".jpg", 
+            ".png", 
+            ".bmp", 
+            ".jpeg", 
+            ".tiff" 
+        };
 
-        public string FilePath { get => _filename; set => SetProperty(ref _filename, value); }
+        private string _photosDirectory;
+        private Dictionary<string, string> _photos;
+        private KeyValuePair<string, string> _selectedPhoto;
+        private float _factor;
+        private float _vertical;
+        private float _horizontal;
+        private float _vMax;
+        private float _hMax;
+        private BitmapImage _image;
+
+        public BitmapImage Image
+        {
+            get => _image;
+            set => SetProperty(ref _image, value);
+        }
+        public float HMax
+        {
+            get => _vMax;
+            set => SetProperty(ref _vMax, value);
+        }
+        public float VMax
+        {
+            get => _hMax;
+            set => SetProperty(ref _hMax, value);
+        }
+        public float Vertical
+        {
+            get => _vertical;
+            set => SetProperty(ref _vertical, value);
+        }
+        public float Horizontal
+        {
+            get => _horizontal;
+            set => SetProperty(ref _horizontal, value);
+        }
+
+        public float Factor
+        {
+            get => _factor;
+            set => SetProperty(ref _factor, value);
+        }
 
         public string PhotosDirectory
         {
@@ -22,14 +70,30 @@ namespace ImageViewer.ViewModels
 
         public RelayCommand SearchButtonCommand { get; set; }
 
-        public List<string> SearchResults
+        public Dictionary<string, string> SearchResults
         {
             get => _photos;
             set => SetProperty(ref _photos, value);
         }
 
+        public KeyValuePair<string, string> SelectedPhoto
+        {
+            get => _selectedPhoto;
+            set
+            {
+                SetProperty(ref _selectedPhoto, value);
+
+                var filePath = Path.Combine(PhotosDirectory, _selectedPhoto.Key);
+                Image = new BitmapImage(new Uri(filePath));
+
+                HMax = (int)Image.Width / 2;
+                VMax = (int)Image.Height / 2;
+            }
+        }
+
         public MainWindowViewModel()
         {
+            Factor = 1;
             SearchButtonCommand = new RelayCommand(o => Search());
         }
 
@@ -37,25 +101,21 @@ namespace ImageViewer.ViewModels
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
             folderBrowserDialog.ShowDialog();
+
+            if (string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+            {
+                MessageBox.Show("Папка не выбрана!");
+                return;
+            }
+
             PhotosDirectory = folderBrowserDialog.SelectedPath;
             SearchResults = GetFiles(PhotosDirectory);
         }
 
-        private List<string> GetFiles(string photosDirectory)
-        {
-            List<string> files = new List<string>();
-
-            foreach (var file in Directory.EnumerateFiles(photosDirectory, "*.jpg"))
-            {
-                files.Add(Path.GetFileNameWithoutExtension(file));
-            }
-
-            return files;
-        }
-
-        public void ImageSelected(string fileName)
-        {
-            FilePath = $"{PhotosDirectory} + {fileName}.jpg";
-        }
+        private static Dictionary<string, string> GetFiles(string photosDirectory) => Directory
+            .EnumerateFiles(photosDirectory)
+            .Select(fn => (fn, ext: Path.GetExtension(fn)))
+            .Where(x => _supportedExtensions.Contains(x.ext))
+            .ToDictionary(x => x.fn, x => Path.GetFileNameWithoutExtension(x.fn));
     }
 }
